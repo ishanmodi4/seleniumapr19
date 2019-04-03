9ea72dee54ec31c040cec81a51fe6e76a1b218d8
ishan_modi <ishan_modi@Jiya>
2019-04-03 13:12:55 -0500

    pushing the code for change request number #4321

---
+++ newjavaproject/src/newjavapackage/newclass.java
@@ -0,0 +1,12 @@
+package newjavapackage;
+
+import newjavapackage.Calculator;
+public class newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator basicCal = new Calculator();
+basicCal.add(50, 10); 
+	}
+
+}
